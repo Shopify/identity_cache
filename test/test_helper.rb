bed85012842cc36ad4e2f11725f69ea1ca16da07
Tom Burns <tom.burns@jadedpixel.com>
2013-05-23 17:35:30 -0400

    Refactor to allow reuse of AR models

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,7 @@ require 'mocha/setup'
 require 'active_record'
 require 'helpers/cache'
 require 'helpers/database_connection'
-
-require_relative 'helpers/cache'
-require_relative 'helpers/database_connection'
+require 'helpers/active_record_objects'
 
 require File.dirname(__FILE__) + '/../lib/identity_cache'
 
@@ -32,6 +30,8 @@ class ActiveSupport::Cache::MemCacheStore
 end
 
 class IdentityCache::TestCase < MiniTest::Unit::TestCase
+  include ActiveRecordObjects
+
   def setup
     DatabaseConnection.drop_tables
     DatabaseConnection.create_tables
@@ -41,13 +41,7 @@ class IdentityCache::TestCase < MiniTest::Unit::TestCase
 
   def teardown
     IdentityCache.cache.clear
-    ActiveSupport::DescendantsTracker.clear
-    ActiveSupport::Dependencies.clear
-    Object.send :remove_const, 'DeeplyAssociatedRecord'
-    Object.send :remove_const, 'PolymorphicRecord'
-    Object.send :remove_const, 'AssociatedRecord'
-    Object.send :remove_const, 'NotCachedRecord'
-    Object.send :remove_const, 'Record'
+    teardown_models
   end
 
   def assert_nothing_raised
@@ -92,38 +86,6 @@ class IdentityCache::TestCase < MiniTest::Unit::TestCase
 
   def cache_hash(key)
     CityHash.hash64(key)
-  end
-
-  private
-  def setup_models(base = ActiveRecord::Base)
-    Object.send :const_set, 'DeeplyAssociatedRecord', Class.new(base).tap {|klass|
-      klass.send :include, IdentityCache
-      klass.belongs_to :associated_record
-    }
-
-    Object.send :const_set, 'AssociatedRecord', Class.new(base).tap {|klass|
-      klass.send :include, IdentityCache
-      klass.belongs_to :record
-      klass.has_many :deeply_associated_records, :order => "name DESC"
-    }
-
-    Object.send :const_set, 'NotCachedRecord', Class.new(base).tap {|klass|
-      klass.belongs_to :record, :touch => true
-    }
-
-    Object.send :const_set, 'PolymorphicRecord', Class.new(base).tap {|klass|
-      klass.belongs_to :owner, :polymorphic => true
-    }
-
-    Object.send :const_set, 'Record', Class.new(base).tap {|klass|
-      klass.send :include, IdentityCache
-      klass.belongs_to :record
-      klass.has_many :associated_records, :order => "id DESC"
-      klass.has_many :not_cached_records, :order => "id DESC"
-      klass.has_many :polymorphic_records, :as => 'owner'
-      klass.has_one :polymorphic_record, :as => 'owner'
-      klass.has_one :associated, :class_name => 'AssociatedRecord', :order => "id ASC"
-    }
   end
 end
 
