acfd9bfbb92baf9573b864cbce22203ec0cb16b6
Arthur Neves <arthurnn@gmail.com>
2013-11-28 16:38:17 -0500

    No need to tab the Class after creating it

--- test/helpers/active_record_objects.rb
+++ test/helpers/active_record_objects.rb
@@ -18,39 +18,39 @@ end
 module ActiveRecordObjects
 
   def setup_models(base = ActiveRecord::Base)
-    Object.send :const_set, 'DeeplyAssociatedRecord', Class.new(base).tap {|klass|
-      klass.send :include, IdentityCache
-      klass.belongs_to :associated_record
+    Object.send :const_set, 'DeeplyAssociatedRecord', Class.new(base) {
+      include IdentityCache
+      belongs_to :associated_record
     }
 
-    Object.send :const_set, 'AssociatedRecord', Class.new(base).tap {|klass|
-      klass.send :include, IdentityCache
-      klass.belongs_to :record
-      klass.has_many :deeply_associated_records, :order => "name DESC"
+    Object.send :const_set, 'AssociatedRecord', Class.new(base) {
+      include IdentityCache
+      belongs_to :record
+      has_many :deeply_associated_records, :order => "name DESC"
     }
 
-    Object.send :const_set, 'NormalizedAssociatedRecord', Class.new(base).tap {|klass|
-      klass.send :include, IdentityCache
-      klass.belongs_to :record
+    Object.send :const_set, 'NormalizedAssociatedRecord', Class.new(base) {
+      include IdentityCache
+      belongs_to :record
     }
 
-    Object.send :const_set, 'NotCachedRecord', Class.new(base).tap {|klass|
-      klass.belongs_to :record, :touch => true
+    Object.send :const_set, 'NotCachedRecord', Class.new(base) {
+      belongs_to :record, :touch => true
     }
 
-    Object.send :const_set, 'PolymorphicRecord', Class.new(base).tap {|klass|
-      klass.belongs_to :owner, :polymorphic => true
+    Object.send :const_set, 'PolymorphicRecord', Class.new(base) {
+      belongs_to :owner, :polymorphic => true
     }
 
-    Object.send :const_set, 'Record', Class.new(base).tap {|klass|
-      klass.send :include, IdentityCache
-      klass.belongs_to :record
-      klass.has_many :associated_records, :order => "id DESC"
-      klass.has_many :normalized_associated_records, :order => "id DESC"
-      klass.has_many :not_cached_records, :order => "id DESC"
-      klass.has_many :polymorphic_records, :as => 'owner'
-      klass.has_one :polymorphic_record, :as => 'owner'
-      klass.has_one :associated, :class_name => 'AssociatedRecord', :order => "id ASC"
+    Object.send :const_set, 'Record', Class.new(base) {
+      include IdentityCache
+      belongs_to :record
+      has_many :associated_records, :order => "id DESC"
+      has_many :normalized_associated_records, :order => "id DESC"
+      has_many :not_cached_records, :order => "id DESC"
+      has_many :polymorphic_records, :as => 'owner'
+      has_one :polymorphic_record, :as => 'owner'
+      has_one :associated, :class_name => 'AssociatedRecord', :order => "id ASC"
     }
   end
 
