bcb722ef7be1d5285d8a98b5b5a43d3525b9213b
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-02-16 01:58:41 +0100

    belongs_to_caching.rb: support {embed: true}, see `build_embedded_belongs_to_cache`

--- lib/identity_cache/belongs_to_caching.rb
+++ lib/identity_cache/belongs_to_caching.rb
@@ -16,15 +16,26 @@ module IdentityCache
         options[:foreign_key]             ||= reflect_on_association(association).foreign_key
         options[:association_class]       ||= reflect_on_association(association).klass
         options[:prepopulate_method_name] ||= "prepopulate_fetched_#{association}"
-        if options[:embed]
-          raise NotImplementedError
-        else
-          build_normalized_belongs_to_cache(association, options)
-        end
+
+        build_embedded_belongs_to_cache(association, options) if options[:embed]
+
+        build_normalized_belongs_to_cache(association, options)
+      end
+
+      def build_embedded_belongs_to_cache(association, options)
+        self.class_eval <<-CODE, __FILE__, __LINE__ + 1
+          def #{association}
+            if IdentityCache.should_cache? && #{options[:foreign_key]}.present? && !association(:#{association}).loaded?
+              self.#{association} = #{options[:association_class]}.fetch_by_id(#{options[:foreign_key]})
+            else
+              super
+            end
+          end
+        CODE
       end
 
       def build_normalized_belongs_to_cache(association, options)
-        self.class_eval(ruby = <<-CODE, __FILE__, __LINE__ + 1)
+        self.class_eval <<-CODE, __FILE__, __LINE__ + 1
           def #{options[:cached_accessor_name]}
             if IdentityCache.should_cache? && #{options[:foreign_key]}.present? && !association(:#{association}).loaded?
               self.#{association} = #{options[:association_class]}.fetch_by_id(#{options[:foreign_key]})
