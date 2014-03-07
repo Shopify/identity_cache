ffb0899aa353381cba055f52a6078425fc4b7dc7
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-03-07 13:41:36 -0500

    profile: Support running a specific runner and saving to a filename.
    A common workflow would be to run the benchmarks for regression testing,
    then run the profile to find out why things are slower.  In this case, we
    want to profile a specific runner which was most affected by a regression.
    The profile identifies which methods are taking the most time, but we often
    want to run stackprof with a --method option to find out which lines it
    spends the most time in, so we should be able to save the stackprof dump.
    
    Example usage:
      rake profile:run RUNNER=FetchNormalizedHitRunner FILENAME=stackprof-cpu.dump

--- performance/profile.rb
+++ performance/profile.rb
@@ -6,13 +6,14 @@ require_relative 'cache_runner'
 
 RUNS = 1000
 
-def run(obj)
+def run(obj, filename: nil)
   puts "#{obj.class.name}:"
   obj.prepare
   data = StackProf.run(mode: :cpu) do
     obj.run
   end
   StackProf::Report.new(data).print_text(false, 20)
+  File.write(filename, Marshal.dump(data)) if filename
   puts
 ensure
   obj.cleanup
@@ -20,6 +21,15 @@ end
 
 create_database(RUNS)
 
-CACHE_RUNNERS.each do |runner|
-  run(runner.new(RUNS))
+if runner_name = ENV['RUNNER']
+  if runner = CACHE_RUNNERS.find{ |r| r.name == runner_name }
+    run(runner.new(RUNS), filename: ENV['FILENAME'])
+  else
+    puts "Couldn't find cache runner #{runner_name.inspect}"
+    exit 1
+  end
+else
+  CACHE_RUNNERS.each do |runner|
+    run(runner.new(RUNS))
+  end
 end
