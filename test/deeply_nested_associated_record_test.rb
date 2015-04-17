18b1e3f34d9dde7cd23d6cfb8fef9e630eac0d7e
Stefano B <stefano.ballabeni+github@gmail.com>
2015-04-17 12:56:43 -0400

    add regression test

---
+++ test/deeply_nested_associated_record_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class DeeplyNestedAssociatedRecordHasOneTest < IdentityCache::TestCase
+  def test_deeply_nested_models_can_cache_has_one_associations
+    assert_nothing_raised do
+      Deeply::Nested::AssociatedRecord.has_one :polymorphic_record, as: 'owner'
+      Deeply::Nested::AssociatedRecord.cache_has_one :polymorphic_record, inverse_name: :owner
+    end
+  end
+
+  def test_deeply_nested_models_can_cache_has_many_associations
+    assert_nothing_raised do
+      Deeply::Nested::AssociatedRecord.has_many :polymorphic_records, as: 'owner'
+      Deeply::Nested::AssociatedRecord.cache_has_many :polymorphic_records, inverse_name: :owner
+    end
+  end
+end
