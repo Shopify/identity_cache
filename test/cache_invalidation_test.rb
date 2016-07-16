51072f063d7669757e450154bc7b17e2d7bc2c54
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2016-07-16 17:15:29 -0400

    Fix another test to make sure it still tests the cache fetch
    Otherwise, that test will just be using the loaded active record
    association

--- test/cache_invalidation_test.rb
+++ test/cache_invalidation_test.rb
@@ -46,14 +46,14 @@ class CacheInvalidationTest < IdentityCache::TestCase
   def test_after_a_reload_the_cache_perform_as_expected
     Item.cache_has_many :associated_records, :embed => :ids
 
-    assert_equal [@baz, @bar], @record.associated_records
     assert_equal [@baz, @bar], @record.fetch_associated_records
+    assert_equal [@baz, @bar], @record.associated_records
 
     @baz.destroy
     @record.reload
 
-    assert_equal [@bar], @record.associated_records
     assert_equal [@bar], @record.fetch_associated_records
+    assert_equal [@bar], @record.associated_records
   end
 
   def test_cache_invalidation_expire_properly_if_child_is_embed_in_multiple_parents
