1ff7cc1a6e725c9590711aa02db1a9e4de75bc4d
Eugene Kenny <elkenny@gmail.com>
2016-09-04 12:14:29 +0100

    Allow cache reads inside unjoinable transactions
    An unjoinable transaction forces any transactions nested inside it to
    behave as if they were at the top level: after_commit callbacks for
    records modified during the nested transaction will be executed when it
    commits, even though the updated data won't be visible to other
    connections until the outer transaction has also committed.
    
    Rails creates unjoinable transactions in two places: in the sandbox
    console, and around transactional tests. In both of these cases, the
    user expects their code to behave as if the transaction wasn't there;
    with this change, IdentityCache will conform to that expectation by only
    disabling cache reads inside joinable transactions.
    
    IdentityCache currently never reads from the cache during transactional
    tests, since there is always at least one transaction open. With this
    change transactional tests behave more like production code, which will
    help catch issues like updating a record that came from the cache.

--- test/identity_cache_test.rb
+++ test/identity_cache_test.rb
@@ -9,16 +9,46 @@ class IdentityCacheTest < IdentityCache::TestCase
   class BadModel < BadModelBase
   end
 
-  class ModelWithConnection < ActiveRecord::Base
-    establish_connection ActiveRecord::Base.connection_config
-  end
-
   def test_identity_cache_raises_if_loaded_twice
     assert_raises(IdentityCache::AlreadyIncludedError) do
       BadModel.class_eval do
         include IdentityCache
       end
     end
+  end
+end
+
+class IdentityCacheWithTransactionalFixturesTest < ActiveSupport::TestCase
+  include ActiveRecord::TestFixtures
+  self.use_transactional_tests = true
+
+  class ModelWithConnection < ActiveRecord::Base
+    establish_connection ActiveRecord::Base.connection_config
+  end
+
+  def test_should_use_cache_outside_transaction
+    assert_equal(true, IdentityCache.should_use_cache?)
+  end
+
+  def test_should_use_cache_in_transaction
+    ActiveRecord::Base.transaction do
+      assert_equal false, IdentityCache.should_use_cache?
+    end
+  end
+
+  def test_should_use_cache_in_transaction_on_specific_model
+    ModelWithConnection.transaction do
+      assert_equal false, IdentityCache.should_use_cache?
+    end
+  end
+end
+
+class IdentityCacheWithoutTransactionalFixturesTest < ActiveSupport::TestCase
+  include ActiveRecord::TestFixtures
+  self.use_transactional_tests = false
+
+  class ModelWithConnection < ActiveRecord::Base
+    establish_connection ActiveRecord::Base.connection_config
   end
 
   def test_should_use_cache_outside_transaction
