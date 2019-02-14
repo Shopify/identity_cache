3dafc541437145a6cfaadf971bb279c7e649fc33
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2019-02-14 17:01:01 -0500

    Fix resetting of inverse association when lazily installing expiry hooks
    ParentModelExpiration#check_association will set the :inverse_name on
    the cached association options when the parent expiry hook is installed
    when the inverse association isn't already loaded.  So we can't depend
    on it already being set during fetching.
    
    However, we can get this information from the active record association
    reflection object. This is even better, because it is the information
    used by active record to set the inverse association that this code is
    trying to undo.

--- lib/identity_cache/query_api.rb
+++ lib/identity_cache/query_api.rb
@@ -246,8 +246,8 @@ module IdentityCache
             record.send(:set_embedded_association, association_name, target)
             association.reset
             # reset inverse associations
-            if target
-              inverse_name = options.fetch(:inverse_name)
+            if target && association_reflection.has_inverse?
+              inverse_name = association_reflection.inverse_of.name
               if target.is_a?(Array)
                 target.each { |child_record| child_record.association(inverse_name).reset }
               else
