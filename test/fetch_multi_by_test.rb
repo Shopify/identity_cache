45a135e7acf437fa8fc50264bb5bca0a0b6d376c
Matt Scriven <matt.scriven@shopify.com>
2019-03-19 13:11:26 -0400

    Add tests for not found scenarios

--- test/fetch_multi_by_test.rb
+++ test/fetch_multi_by_test.rb
@@ -25,6 +25,15 @@ class FetchMultiByTest < IdentityCache::TestCase
     assert_equal [@bob, @bertha], Item.fetch_multi_by_title(['bob', 'bertha'])
   end
 
+  def test_fetch_multi_by_cache_key_with_unknown_key
+    Item.cache_index :title, unique: false
+
+    @bob.save!
+    @bertha.save!
+
+    assert_equal [@bob], Item.fetch_multi_by_title(['bob', 'garbage_title'])
+  end
+
   def test_fetch_multi_by_unique_cache_key
     Item.cache_index :title, unique: true
 
@@ -47,6 +56,14 @@ class FetchMultiByTest < IdentityCache::TestCase
     assert_equal({ 1 => ['bob'], 2 => ['bertha'] }, Item.fetch_multi_title_by_id([1, 2]))
   end
 
+  def test_fetch_multi_attribute_by_cache_key_with_unknown_key
+    Item.cache_attribute :title, by: :id, unique: false
+
+    @bob.save!
+    @bertha.save!
+
+    assert_equal({ 1 => ['bob'], 999 => [] }, Item.fetch_multi_title_by_id([1, 999]))
+  end
 
   def test_fetch_multi_attribute_by_unique_cache_key
     Item.cache_attribute :title, by: :id, unique: true
@@ -57,5 +74,14 @@ class FetchMultiByTest < IdentityCache::TestCase
     assert_equal 'bob', Item.fetch_title_by_id(1)
 
     assert_equal({ 1 => 'bob', 2 => 'bertha' }, Item.fetch_multi_title_by_id([1, 2]))
+  end
+
+  def test_fetch_multi_attribute_by_unique_cache_key_with_unknown_key
+    Item.cache_attribute :title, by: :id, unique: true
+
+    @bob.save!
+    @bertha.save!
+
+    assert_equal({ 1 => 'bob', 999 => nil }, Item.fetch_multi_title_by_id([1, 999]))
   end
 end
