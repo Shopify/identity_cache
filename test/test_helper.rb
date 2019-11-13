12afc5c7f4ad05ce9b2291331d4111db5c45f106
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2019-11-12 19:22:09 -0500

    Remove Rails <5 raise_in_transactional_callbacks code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -33,11 +33,6 @@ class IdentityCache::TestCase < Minitest::Test
   attr_reader :backend
 
   def setup
-    if ActiveRecord.gem_version < Gem::Version.new('5') &&
-       ActiveRecord::Base.respond_to?(:raise_in_transactional_callbacks=)
-      ActiveRecord::Base.raise_in_transactional_callbacks = true
-    end
-
     DatabaseConnection.drop_tables
     DatabaseConnection.create_tables
 
