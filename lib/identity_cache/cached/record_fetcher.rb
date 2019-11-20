e18ee38a7010e10782eff681957935da62359d84
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2019-11-20 17:31:52 -0500

    Move methods out of QueryAPI that are only needed for the RecordFetcher

--- lib/identity_cache/cached/record_fetcher.rb
+++ lib/identity_cache/cached/record_fetcher.rb
@@ -36,13 +36,13 @@ module IdentityCache
       def load_multi_from_db(ids)
         ids = ids.map { |id| model.connection.type_cast(id, id_column) }
         records = model.where(model.primary_key => ids).includes(db_load_includes).to_a
-        setup_embedded_associations_on_miss(records)
+        self.class.setup_embedded_associations_on_miss(model, records)
         records.index_by(&:id)
       end
 
       def load_from_db(id)
         record = model.includes(db_load_includes).where(model.primary_key => id).take
-        setup_embedded_associations_on_miss([record]) if record
+        self.class.setup_embedded_associations_on_miss(model, [record]) if record
         record
       end
 
@@ -51,7 +51,19 @@ module IdentityCache
       protected
 
       def db_load_includes
-        model.send(:cache_fetch_includes)
+        @db_load_includes ||= begin
+          model.recursively_embedded_associations.map do |child_association, _options|
+            child_class = model.reflect_on_association(child_association).try(:klass)
+
+            child_includes = child_class.cached_record_fetcher.db_load_includes
+
+            if child_includes.blank?
+              child_association
+            else
+              { child_association => child_includes }
+            end
+          end
+        end
       end
 
       private
@@ -60,9 +72,92 @@ module IdentityCache
         @id_column ||= model.columns.detect { |c| c.name == model.primary_key }
       end
 
+      class << self
+        def preload_id_embedded_association(model, records, cached_association)
+          reflection = cached_association.reflection
+          child_model = reflection.klass
+          scope = child_model.all
+          scope = scope.where(reflection.type => model.base_class.name) if reflection.type
+          scope = scope.instance_exec(nil, &reflection.scope) if reflection.scope
 
-      def setup_embedded_associations_on_miss(records)
-        model.send(:setup_embedded_associations_on_miss, records)
+          pairs = scope.where(reflection.foreign_key => records.map(&:id)).pluck(
+            reflection.foreign_key, reflection.association_primary_key
+          )
+          ids_by_parent = Hash.new { |hash, key| hash[key] = [] }
+          pairs.each do |parent_id, child_id|
+            ids_by_parent[parent_id] << child_id
+          end
+
+          records.each do |parent|
+            child_ids = ids_by_parent[parent.id]
+            case cached_association
+            when Cached::Reference::HasMany
+              parent.instance_variable_set(cached_association.ids_variable_name, child_ids)
+            when Cached::Reference::HasOne
+              parent.instance_variable_set(cached_association.id_variable_name, child_ids.first)
+            end
+          end
+        end
+
+        def each_id_embedded_association(model)
+          model.cached_has_manys.each_value do |association|
+            yield association if association.embedded_by_reference?
+          end
+          model.cached_has_ones.each_value do |association|
+            yield association if association.embedded_by_reference?
+          end
+        end
+
+        def setup_embedded_associations_on_miss(model, records,
+          readonly: IdentityCache.fetch_read_only_records && model.should_use_cache?
+        )
+          return if records.empty?
+          records.each(&:readonly!) if readonly
+          each_id_embedded_association(model) do |cached_association|
+            preload_id_embedded_association(model, records, cached_association)
+          end
+          model.recursively_embedded_associations.each_value do |cached_association|
+            association_reflection = cached_association.reflection
+            association_name = association_reflection.name
+
+            # Move the loaded records to the cached association instance variable so they
+            # behave the same way if they were loaded from the cache
+            records.each do |record|
+              association = record.association(association_name)
+              target = association.target
+              target = readonly_copy(target) if readonly
+              record.set_embedded_association(association_name, target)
+              association.reset
+              # reset inverse associations
+              if target && association_reflection.has_inverse?
+                inverse_name = association_reflection.inverse_of.name
+                if target.is_a?(Array)
+                  target.each { |child_record| child_record.association(inverse_name).reset }
+                else
+                  target.association(inverse_name).reset
+                end
+              end
+            end
+
+            child_model = association_reflection.klass
+            child_records = records.flat_map(&cached_association.cached_accessor_name).compact
+            setup_embedded_associations_on_miss(child_model, child_records, readonly: readonly)
+          end
+        end
+
+        def readonly_record_copy(record)
+          record = record.clone
+          record.readonly!
+          record
+        end
+
+        def readonly_copy(record_or_records)
+          if record_or_records.is_a?(Array)
+            record_or_records.map { |record| readonly_record_copy(record) }
+          elsif record_or_records
+            readonly_record_copy(record_or_records)
+          end
+        end
       end
     end
   end
--- lib/identity_cache/query_api.rb
+++ lib/identity_cache/query_api.rb
@@ -102,93 +102,8 @@ module IdentityCache
         end
       end
 
-      def preload_id_embedded_association(records, cached_association)
-        reflection = cached_association.reflection
-        child_model = reflection.klass
-        scope = child_model.all
-        scope = scope.where(reflection.type => base_class.name) if reflection.type
-        scope = scope.instance_exec(nil, &reflection.scope) if reflection.scope
-
-        pairs = scope.where(reflection.foreign_key => records.map(&:id)).pluck(
-          reflection.foreign_key, reflection.association_primary_key
-        )
-        ids_by_parent = Hash.new { |hash, key| hash[key] = [] }
-        pairs.each do |parent_id, child_id|
-          ids_by_parent[parent_id] << child_id
-        end
-
-        records.each do |parent|
-          child_ids = ids_by_parent[parent.id]
-          case cached_association
-          when Cached::Reference::HasMany
-            parent.instance_variable_set(cached_association.ids_variable_name, child_ids)
-          when Cached::Reference::HasOne
-            parent.instance_variable_set(cached_association.id_variable_name, child_ids.first)
-          end
-        end
-      end
-
-      def setup_embedded_associations_on_miss(records,
-        readonly: IdentityCache.fetch_read_only_records && should_use_cache?
-      )
-        return if records.empty?
-        records.each(&:readonly!) if readonly
-        each_id_embedded_association do |cached_association|
-          preload_id_embedded_association(records, cached_association)
-        end
-        recursively_embedded_associations.each_value do |cached_association|
-          association_reflection = cached_association.reflection
-          association_name = association_reflection.name
-
-          # Move the loaded records to the cached association instance variable so they
-          # behave the same way if they were loaded from the cache
-          records.each do |record|
-            association = record.association(association_name)
-            target = association.target
-            target = readonly_copy(target) if readonly
-            record.send(:set_embedded_association, association_name, target)
-            association.reset
-            # reset inverse associations
-            if target && association_reflection.has_inverse?
-              inverse_name = association_reflection.inverse_of.name
-              if target.is_a?(Array)
-                target.each { |child_record| child_record.association(inverse_name).reset }
-              else
-                target.association(inverse_name).reset
-              end
-            end
-          end
-
-          child_model = association_reflection.klass
-          child_records = records.flat_map(&cached_association.cached_accessor_name).compact
-          child_model.send(:setup_embedded_associations_on_miss, child_records, readonly: readonly)
-        end
-      end
-
-      def readonly_record_copy(record)
-        record = record.clone
-        record.readonly!
-        record
-      end
-
-      def readonly_copy(record_or_records)
-        if record_or_records.is_a?(Array)
-          record_or_records.map { |record| readonly_record_copy(record) }
-        elsif record_or_records
-          readonly_record_copy(record_or_records)
-        end
-      end
-
-      def each_id_embedded_association
-        cached_has_manys.each_value do |association|
-          yield association if association.embedded_by_reference?
-        end
-        cached_has_ones.each_value do |association|
-          yield association if association.embedded_by_reference?
-        end
-      end
-
-      def recursively_embedded_associations
+      # @api private
+      public def recursively_embedded_associations # :nodoc:
         all_cached_associations.select { |_name, association| association.embedded_recursively? }
       end
 
@@ -198,22 +113,6 @@ module IdentityCache
 
       def embedded_associations
         all_cached_associations.select { |_name, association| association.embedded? }
-      end
-
-      def cache_fetch_includes
-        associations_for_identity_cache = recursively_embedded_associations.map do |child_association, _options|
-          child_class = reflect_on_association(child_association).try(:klass)
-
-          child_includes = child_class.send(:cache_fetch_includes)
-
-          if child_includes.blank?
-            child_association
-          else
-            { child_association => child_includes }
-          end
-        end
-
-        associations_for_identity_cache.compact
       end
     end
 
@@ -254,7 +153,8 @@ module IdentityCache
       end
     end
 
-    def set_embedded_association(association_name, association_target) #:nodoc:
+    # @api private
+    public def set_embedded_association(association_name, association_target) #:nodoc:
       model = self.class
       cached_association = model.cached_association(association_name)
 
